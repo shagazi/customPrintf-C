5eae7643e208c727c1cacd645de2e31f4c2c3ea3
Shreef Hagazi <shagazi@e1z2r4p26.42.us.org>
2018-06-01 18:39:21 -0700

    remove main

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: shagazi <shagazi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/16 20:29:12 by shagazi           #+#    #+#             */
-/*   Updated: 2018/06/01 18:34:34 by shagazi          ###   ########.fr       */
+/*   Updated: 2018/06/01 18:39:10 by shagazi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,12 +110,12 @@ int ft_printf(char *format, ...)
 	free(fmt);
 	return (k);
 }
-
-int main()
-{
-	int x = ft_printf("% 5s\n", NULL);
-	int y = printf("% 5s\n", NULL);
-
-	printf("%d\n", x);
-	printf("%d\n", y);
-}
+// 
+// int main()
+// {
+// 	int x = ft_printf("% 5s\n", NULL);
+// 	int y = printf("% 5s\n", NULL);
+//
+// 	printf("%d\n", x);
+// 	printf("%d\n", y);
+// }
